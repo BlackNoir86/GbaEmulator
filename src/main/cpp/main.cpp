45c7cfc22ed431fa3f0f9b948a5c372b2abbdab9
BlackNoir86 <alessio.compagnone8@gmail.com>
2026-08-12 11:12:53 +0200

    Update main.cpp

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,8 +1,43 @@
 #include <android/native_activity.h>
+#include <android/native_window.h>
 #include <android/log.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "GbaEmulator", __VA_ARGS__)
 
+static void onNativeWindowCreated(ANativeActivity* activity, ANativeWindow* window) {
+    LOGI("Finestra Nativa Creata! Inizializzo EGL/OpenGL...");
+
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    eglInitialize(display, nullptr, nullptr);
+
+    const EGLint attribs[] = {
+        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+        EGL_BLUE_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_RED_SIZE, 8,
+        EGL_NONE
+    };
+
+    EGLConfig config;
+    EGLint numConfigs;
+    eglChooseConfig(display, attribs, &config, 1, &numConfigs);
+
+    EGLSurface surface = eglCreateWindowSurface(display, config, window, nullptr);
+
+    EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
+    EGLContext context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+
+    eglMakeCurrent(display, surface, surface, context);
+
+    // Coloriamo lo schermo di Blu GBA (R: 0.1, G: 0.2, B: 0.5)
+    glClearColor(0.1f, 0.2f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    eglSwapBuffers(display, surface);
+}
+
 void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
     LOGI("GBA Emulator Native Activity Started!");
+    activity->callbacks->onNativeWindowCreated = onNativeWindowCreated;
 }
