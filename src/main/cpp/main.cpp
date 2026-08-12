cc89dcb894944ccd3fc4873c727c747007bfff5c
BlackNoi86 <alessio.compagnone8@gmail.com>
2026-08-12 13:52:17 +0200

    Aggiunto ponte JNI per caricamento ROM

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,43 +1,18 @@
-#include <android/native_activity.h>
-#include <android/native_window.h>
+#include <jni.h>
+#include <vector>
 #include <android/log.h>
-#include <EGL/egl.h>
-#include <GLES2/gl2.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "GbaEmulator", __VA_ARGS__)
+#define LOG_TAG "GBA_CORE"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-static void onNativeWindowCreated(ANativeActivity* activity, ANativeWindow* window) {
-    LOGI("Finestra Nativa Creata! Inizializzo EGL/OpenGL...");
+std::vector<uint8_t> romBuffer;
 
-    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    eglInitialize(display, nullptr, nullptr);
-
-    const EGLint attribs[] = {
-        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-        EGL_BLUE_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_RED_SIZE, 8,
-        EGL_NONE
-    };
-
-    EGLConfig config;
-    EGLint numConfigs;
-    eglChooseConfig(display, attribs, &config, 1, &numConfigs);
-
-    EGLSurface surface = eglCreateWindowSurface(display, config, window, nullptr);
-
-    EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
-    EGLContext context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
-
-    eglMakeCurrent(display, surface, surface, context);
-
-    // Coloriamo lo schermo di Blu GBA (R: 0.1, G: 0.2, B: 0.5)
-    glClearColor(0.1f, 0.2f, 0.5f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    eglSwapBuffers(display, surface);
-}
-
-void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
-    LOGI("GBA Emulator Native Activity Started!");
-    activity->callbacks->onNativeWindowCreated = onNativeWindowCreated;
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_example_gbaemulator_MainActivity_loadRomNative(JNIEnv *env, jobject instance, jbyteArray romData) {
+    jsize length = env->GetArrayLength(romData);
+    romBuffer.resize(length);
+    env->GetByteArrayRegion(romData, 0, length, reinterpret_cast<jbyte*>(romBuffer.data()));
+    
+    LOGI("ROM caricata con successo! Dimensione: %d byte", length);
+    return JNI_TRUE;
 }
