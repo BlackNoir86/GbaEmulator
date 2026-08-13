f98c9b480676db11469b9c3d12401fb19f01aa09
BlackNoi86 <alessio.compagnone8@gmail.com>
2026-08-13 11:33:48 +0200

    UI: Aggiunto layout con controlli Game Boy Advance

--- app/src/main/java/com/example/gbaemulator/MainActivity.kt
+++ app/src/main/java/com/example/gbaemulator/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.gbaemulator
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this)
-        tv.text = "GBA Emulator - Avvio Riuscito!"
-        tv.textSize = 24f
-        tv.setPadding(32, 64, 32, 32)
-        setContentView(tv)
+        setContentView(R.layout.activity_main)
     }
 }
